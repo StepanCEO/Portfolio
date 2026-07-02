deab189f4f5cb34c28193b046f39a087bcb23921
Lolhcs-dot <lolhcs@mail.ru>
2026-07-02 21:16:56 +0300

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        app: resolve(__dirname, 'app.html'),
+      },
+    },
+  },
 })
